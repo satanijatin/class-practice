2e1cace523a36e5ac8066490bcb0c2e0fa7c5780
Jatin Satani <jatinsatani111@gmail.com>
2024-03-20 14:24:41 +0530

    Done

--- 01Feb_python/Django_crud/project_crud/crudapp/views.py
+++ 01Feb_python/Django_crud/project_crud/crudapp/views.py
@@ -2,75 +2,109 @@ from django.shortcuts import render,redirect,get_object_or_404
 from .models import Student
 import os
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
-def reg(request):
 
+
+def reg(request): 
     if request.method=='POST':
-        
-        if request.POST['user_id']!="":
-            user_id = request.POST['user_id']
-            data = request.POST
-            select_user = Student.objects.get(id=user_id)
-            select_user.uname = data.get('uname')
-            select_user.email = data.get('email')
-            select_user.password = data.get('password')
-            select_user.gender = data.get('gender')
-            
-            str_lan = ",".join(str(element) for element in request.POST.getlist("lng"))
-            
-            print(str_lan)
-                      
-            select_user.country = data.get('country')
-                                 
-            # try:    
-            #         select_user.lang=str_lan
-            #         os.remove(select_user.img.path)
-            #         select_user.img = request.FILES['img']
-                  
-            #         select_user.save()
-            # except:
-            #         select_user.lang=str_lan
-            #         select_user.save()
-            
-            if len(request.FILES) !=0 :
-                    if select_user.img != "":
-                        os.remove(select_user.img.path)
-                        select_user.img = request.FILES.get('img')
-                    else:
-                        select_user.img = request.FILES.get('img')
-            else:
-                select_user.img=select_user.img
+        if request.POST['form_type']=="insert":
+            if request.POST['user_id']!="":
+                user_id = request.POST['user_id']
+                data = request.POST
+                select_user = Student.objects.get(id=user_id)
+                select_user.uname = data.get('uname')
+                select_user.email = data.get('email')
+                select_user.password = data.get('password')
+                select_user.gender = data.get('gender')
+                
+                str_lan = ",".join(str(element) for element in request.POST.getlist("lng"))
+                
+                print(str_lan)
+                        
+                select_user.country = data.get('country')
+                                    
+                # try:    
+                #         select_user.lang=str_lan
+                #         os.remove(select_user.img.path)
+                #         select_user.img = request.FILES['img']
                     
-            select_user.save()
-                    
-            return redirect('reg')
-        else:
-            data = request.POST
-            uname = data.get('uname')
-            email = data.get('email')
-            password = data.get('password')
-            gender = data.get('gender')
-            lang = data.getlist('lng')
-            country = data.get('country')
-            img = request.FILES.get('img')
-
-        
-            lng=""
-            j=0
-            for i in lang:
-                if j==0:
-                    lng=lng+i
+                #         select_user.save()
+                # except:
+                #         select_user.lang=str_lan
+                #         select_user.save()
+                
+                if len(request.FILES) !=0 :
+                        if select_user.img != "":
+                            os.remove(select_user.img.path)
+                            select_user.img = request.FILES.get('img')
+                        else:
+                            select_user.img = request.FILES.get('img')
                 else:
-                    lng=lng+","+i
-                j=j+1
-          
+                    select_user.img=select_user.img
+                        
+                select_user.save()
+                        
+                return redirect('reg')
+            
+            else:
+                data = request.POST
+                uname = data.get('uname')
+                email = data.get('email')
+                password = data.get('password')
+                gender = data.get('gender')
+                lang = data.getlist('lng')
+                country = data.get('country')
+                img = request.FILES.get('img')
 
-            Student.objects.create(uname=uname,email=email,password=password,gender=gender,lang=lng,country=country,img=img)
-            return redirect('reg')
+            
+                lng=""
+                j=0
+                for i in lang:
+                    if j==0:
+                        lng=lng+i
+                    else:
+                        lng=lng+","+i
+                    j=j+1
+            
+
+                Student.objects.create(uname=uname,email=email,password=password,gender=gender,lang=lng,country=country,img=img)
+                return redirect('reg')
+        else:
+            
+            global datas 
+            if 'q' in request.POST :
+                q = request.POST['q']
+        # data = Data.objects.filter(last_name__icontains=q)
+                multiple_q = Q(Q(uname__icontains=q))
+            
+            
+                datas = Student.objects.filter(multiple_q)          
+            else:
+                datas = Student.objects.all()
+            
+            p = Paginator(datas, 2) 
+            page_number = request.GET.get('page')
+            try:
+                page_obj = p.get_page(page_number) 
+            except PageNotAnInteger:
+                    
+                    page_obj = p.page(1)
+            except EmptyPage:
+                # if page is empty then return last page
+                            page_obj = p.page(p.num_pages)
+                
+            context =  {'alldata':datas,
+                        'page_obj': page_obj,
+                        'total_page':range(1, page_obj.paginator.num_pages+1)
+                    }
+            return render(request, 'reg.html', context)
+            
             
     else:
                
